af7d82490fbbb88126463c0d11c015df47510dca
Gabriel Pessoa <gabrieldeloiolacontato@gmail.com>
2022-11-18 18:20:42 -0300

    Just adding some features to have a real CRUD application. But its  not done yet

--- kotlin-springboot-crud/src/main/kotlin/br/com/projetos/gabrielpessoa/kotlinspringbootcrud/controller/StudentController.kt
+++ kotlin-springboot-crud/src/main/kotlin/br/com/projetos/gabrielpessoa/kotlinspringbootcrud/controller/StudentController.kt
@@ -1,8 +1,12 @@
 package br.com.projetos.gabrielpessoa.kotlinspringbootcrud.controller
 
+import br.com.projetos.gabrielpessoa.kotlinspringbootcrud.model.Student
 import br.com.projetos.gabrielpessoa.kotlinspringbootcrud.service.StudentService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,10 +16,20 @@ class StudentController {
     lateinit var studentService: StudentService
 
 
-    @GetMapping
-
-
-
-
-
+    //read
+    @GetMapping("/allStudents")
+    fun findAllStudents( ) : List<Student>{
+        return studentService.findAllStudents()
+    }
+    //create
+    @PostMapping("addStudents")
+    fun  addStudent(@RequestBody addStudents:Student): Student {
+        return studentService.addStudent(addStudents)
+    }
+    //delete
+    @DeleteMapping("/deleteStudent")
+    fun deleteStudent(@RequestBody dltStudent:Student) {
+        return studentService.deleteStudent(dltStudent)
+    }
 }
+
